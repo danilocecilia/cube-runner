b8f8cd30a2bd6cef08fc620460f3cefda376be78
Danilo Cecilia <danilo.cecilia@zenabis.com>
2020-10-23 21:33:42 -0700

    First Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+  bool gameHasEnded = false;
+  public float delayRestart = 1f;
+
+  public GameObject completeLevelUI;
+
+  public void EndGame()
+  {
+    if (!gameHasEnded)
+    {
+      gameHasEnded = true;
+      Debug.Log("Game Over!");
+      Invoke("Restart", delayRestart);
+    }
+  }
+
+  public void CompleteLevel()
+  {
+    completeLevelUI.SetActive(true);
+    Debug.Log("Level Completed!");
+  }
+
+  void Restart()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+  public Rigidbody rb;
+
+  // Start is called before the first frame update
+  // void Start()
+  // {
+  //   rb.AddForce(0, 200, 500);
+  // }
+
+  public float forwardForce = 2000f;
+  public float sidewaysForce = 500f;
+
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+    if (Input.GetKey("d"))
+    {
+      rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    if (Input.GetKey("a"))
+    {
+      rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    if (rb.position.y < -1f)
+    {
+      FindObjectOfType<GameManager>().EndGame();
+    }
+  }
+}
